0bb3e40e21015d3ad9801d33b988e09393a77f92
Maxim Kuzmin <43728459+MaxNacemit@users.noreply.github.com>
2018-12-03 17:50:49 +0300

    Добавлены средства работы с комнатами

--- db_api_labyrinth.py
+++ db_api_labyrinth.py
@@ -3,15 +3,15 @@ import sqlite3
 
 class database: 
     def __init__(self):
-        global cursor, conn
         #Создает объект базы данных. Рассчитано на запуск один раз, но проверяет наличие таблиц, чтобы не было ошибки SQL
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
+        self.conn = sqlite3.connect('database.db')
+        self.cursor = conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS users (login text PRIMARY KEY, pass_hash text)''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS maps (id integer PRIMARY KEY, maplink text, description text)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS maps (id integer PRIMARY KEY, maplink text, description text)''') #description - это описание карты, дающееся создателем
+        cursor.execute('CREATE TABLE IF NOT EXISTS rooms (id integer PRIMARY KEY, joinlink text, settings text, description text, players text)') #settings - настройки комнаты, как именно хранятся - пока непонятно, тип text, ибо самый универсальный
         conn.commit()
         
-    def quit():
+    def quit(self):
         conn.close()
         
     def add_user(self, login, pass_hash):
@@ -25,7 +25,10 @@ class database:
         cursor.execute('INSERT INTO maps VALUES (NULL, ?, ?)', map_data)
         conn.commit()
     
-    #Я пока не понимаю, как работать с комнатами с помощью датабазы, поэтому их тут пока не будет
+    def add_room(self, joinlink, settings, description, creator):
+        room_data = [joinlink, settings, description, creator]
+        cursor.execute('INSERT INTO rooms VALUES (NULL, ?, ?, ?, ?)', room_data)
+        conn.commit()
     
     def get_user(self, login):
         #Возвращает данные пользователя с данным логином
@@ -35,5 +38,14 @@ class database:
     def get_map(self, mapid):
         cursor.execute('SELECT * FROM maps WHERE id=?', [mapid])
         return cursor.fetchone()
-
     
+    def get_room(self, roomid):
+        cursor.execute('SELECT * FROM rooms WHERE id=?', [roomid])
+        return cursor.fetchone()
+    
+    def add_player(self, roomid, player):
+        data = get_room(self, roomid)
+        cursor.execute('DELETE * FROM rooms WHERE id=?', [roomid])
+        data[-1] = data[-1] + ' ' + player
+        cursor.execute('INSERT INTO rooms VALUES (?, ?, ?, ?, ?)', data)
+        conn.commit()
