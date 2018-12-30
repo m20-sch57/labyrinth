13f851f964ea187ae6f8528320f7e654b55bb117
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2018-12-30 14:40:00 +0300

    #26 add labyrinths_list

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from database import Database
+from labyrinths_list import LabyrinthsList
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your_secret_string"
 dbase = Database()
+labyrinths_list = LabyrinthsList()
 
 from app import routes
---
+++ labyrinths_list.py
@@ -0,0 +1,28 @@
+class LabyrinthsList:
+	'''
+	Позволит хранить список игр в оперативной памяти
+
+	room_id - это id комнаты в которой происходит игра
+	'''
+	def __init__(self):
+		self.list = {}
+
+
+	def get_labyrinth(room_id):
+		return self.list(room_id)
+
+
+	def add_labyrinth(room_id, labyrinth):
+		if room_id in self.list:
+			return False
+		else:
+			self.list[room_id] = labyrinth 
+			return True
+
+
+	def remove_labyrinth(room_id):
+		if room_id in self.list:
+			return False
+		else:
+			self.list.pop(room_id)
+			return True
