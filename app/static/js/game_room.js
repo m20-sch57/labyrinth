71001872fbd12e1c21cb44136a763266003d7078
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2019-01-06 19:13:54 +0300

    # 45 bug fix

--- app/static/js/game_room.js
+++ app/static/js/game_room.js
@@ -17,29 +17,37 @@ function makeTurn(event) {
 	};
 };
 
-var socket = io.connect('http://' + document.domain + ':' + location.port + '/grws');
-socket.on('update', function(msg) {
-	switch (msg.event) {
-		case 'player_make_turn':
-			var xhr = xhrOpen('update');
-			xhr.send();
-			responseData = JSON.parse(xhr.responseText);
+function getUpdate() {
+	var xhr = xhrOpen('update');
+	xhr.send();
+	responseData = JSON.parse(xhr.responseText);
 
-			var log = document.getElementById('log');
-			log.innerHTML += ('<p>'+ responseData.msg +'</p>');
+	var log = document.getElementById('log');
+	log.innerHTML += ('<p>'+ responseData.msg +'</p>');
 
-			var turnState = document.getElementById('turn_state');
-			if (responseData.your_turn == 'yes') {
-				turnState.innerHTML = 'Твой ход (' + responseData.ats + ')';
-			} else {
-				turnState.innerHTML = 'Подожди';
-			};
-			break;
+	var turnState = document.getElementById('turn_state');
+	if (responseData.your_turn == 'yes') {
+		turnState.innerHTML = 'Твой ход (' + responseData.ats + ')';
+	} else {
+		turnState.innerHTML = 'Подожди';
 	};
-});
-socket.on('connect', function() {
-	socket.emit('player join', {'room_id': document.getElementById('info').getAttribute('room_id')});
-});
+};
 
+function ready() {
+	getUpdate();
+	var socket = io.connect('http://' + document.domain + ':' + location.port + '/grws');
+	socket.on('update', function(msg) {
+		switch (msg.event) {
+			case 'player_make_turn':
+				getUpdate();
+				break;
+		};
+	});
+	socket.on('connect', function() {
+		socket.emit('player join', {'room_id': document.getElementById('info').getAttribute('room_id')});
+	});
+};
 var turnInput = document.getElementById('input');
 turnInput.onkeydown = makeTurn;
+
+document.addEventListener("DOMContentLoaded", ready);
