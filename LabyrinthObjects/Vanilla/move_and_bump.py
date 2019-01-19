02c991f2b872d872b83dd78eaf26b4e21f6b51ff
Lounres <minaevgleb@yandex.ru>
2019-01-19 19:08:36 +0300

    Fixed #49: 5(2, 5).

--- LabyrinthObjects/Vanilla/move_and_bump.py
+++ LabyrinthObjects/Vanilla/move_and_bump.py
@@ -40,31 +40,33 @@ class Outside(Location):
 # Location.
 # Just prototype. But it's more useful than new Wall sometimes...
 class GlobalWall(Location):
-    reverse_direction = {'up': 'down',
-                         'down': 'up',
-                         'left': 'right',
-                         'right': 'left'}
 
-    def __init__(self, arg):
-        if type(arg) is list:
-            new_arg = {}
-            for arr in arg:
-                d0 = new_arg.get(arr[0], {})
-                d0[arr[2]] = arr[1]
-                new_arg[arr[0]] = d0
-                d1 = new_arg.get(arr[1], {})
-                d1[reverse_direction[arr[2]]] = arr[0]
-                new_arg[arr[1]] = d1
-            arg = new_arg
-        # behind_the_wall is dict of dicts. For location with ID i on i-th place will be dict like
+    def __init__(self, *args):
+        # Every argument must be like: (first_loc_i, second_loc_i, dir_from_first_to_second_i).
+
+        self.behind_the_wall = {}
+        for tup in args:
+            if type(tup) is not tuple or len(tup) != 3:
+                raise TypeError('Every argument of GlobalWall must be tuple with 3 items.')
+            if type(tup[2]) is not str:
+                raise TypeError('Every direction in arguments of GlobalWall must be string.')
+            d0 = self.behind_the_wall.get(tup[0], {})
+            if tup[2] in d0:
+                raise ValueError('There are two walls in same direction of some room.')
+            d0[tup[2]] = tup[1]
+            self.behind_the_wall[tup[0]] = d0
+            d1 = self.behind_the_wall.get(tup[1], {})
+            if reverse_direction(tup[2]) in d1:
+                raise ValueError('There are two walls in same direction of some room.')
+            d1[reverse_direction[tup[2]]] = tup[0]
+            self.behind_the_wall[tup[1]] = d1
+        # behind_the_wall is dict of dicts. For location on i-th place will be dict like
         # {'direction1': location_in_direction1_behind_wall, ...}.
         # So all list looks like {loc1: {'dir1': loc_in_dir1, ...}, ...}.
-        # loc and loc_in_dir are integers (not location or ID) too.
-        self.behind_the_wall = arg
 
     def break_wall(self, object_1, direction):
-        if issubclass(Location, object_1):
-            raise ValueError('Invalid literal for break_wall()')
+        if not isinstance(object_1, Location):
+            raise ValueError('First argument of GlobalWall.break_wall must be instance of Location.')
         object_2 = self.behind_the_wall[object_1][direction]
         object_1.set_neighbour(direction, object_2)
         object_2.set_neighbour(reverse_direction[direction], object_1)
@@ -72,21 +74,23 @@ class GlobalWall(Location):
         del self.behind_the_wall[object_2][reverse_direction[direction]]
 
     def make_wall(self, object_1, direction):
-        if issubclass(Location, object_1):
-            raise ValueError('Invalid literal for make_wall()')
+        if not isinstance(object_1, Location):
+            raise ValueError('First argument of GlobalWall.break_wall must be instance of Location.')
         object_2 = object_1.get_neighbour(direction)
 
         d1 = self.behind_the_wall.get(object_1, {})
         if direction in d1:
-            raise ValueError('There is already wall between first room and some another in the direction')
+            raise ValueError('Invalid arguments for GlobalWall.make_wall. '
+                             'There is already wall between first room and some another in the direction.')
         d1[direction] = object_2
         self.behind_the_wall[object_1] = d1
-        object_1.set_nrighbour(direction, self)
+        object_1.set_neighbour(direction, self)
 
         d2 = self.behind_the_wall.get(object_2, {})
-        if self.reverse_direction[direction] in d2:
-            raise ValueError('There is already wall between second room and some another in the opposite direction')
-        d2[self.reverse_direction[direction]] = object_1
+        if reverse_direction[direction] in d2:
+            raise ValueError('Invalid arguments for GlobalWall.make_wall. '
+                             'There is already wall between first room and some another in the direction.')
+        d2[reverse_direction[direction]] = object_1
         self.behind_the_wall[object_2] = d2
         object_2.set_neighbour(reverse_direction[direction], self)
 
@@ -94,26 +98,23 @@ class GlobalWall(Location):
 # Location.
 class Wall(Location):
     def __init__(self, *args):
-        if len(args) == 0:
-            raise ValueError('Invalid literal for Wall')
-        if type(args[0]) is list:
-            if len(args) > 1:
-                raise ValueError('Invalid literal for Wall')
-            args = args[0]
+        # Every argument must be like: (from_loc_i, dir_i, to_loc_i).
 
         self.behind_the_wall = {}
         for i in range(len(args)):
-            pair = args[i]
-            next_pair = args[(i + 1) % len(args)]
-            if type(pair) is not tuple or len(pair) != 2:
-                raise ValueError('Invalid literal for Wall')
-            d = self.behind_the_wall.get(pair[0], {})
-            d[pair[1]] = next_pair[0]
-            self.behind_the_wall[pair[0]] = d
-        # behind_the_wall is dict of dicts. For location with ID i on i-th place will be dict like
+            tup = args[i]
+            if type(tup) is not tuple or len(tup) != 3:
+                raise TypeError('Every argument of Wall must be tuple with 3 items.')
+            if type(tup[1]) is not str:
+                raise TypeError('Every direction in arguments of Wall must be string.')
+            d = self.behind_the_wall.get(tup[0], {})
+            if tup[1] in d:
+                raise ValueError('There are two walls in same direction of some room.')
+            d[tup[1]] = tup[2]
+            self.behind_the_wall[tup[0]] = d
+        # behind_the_wall is dict of dicts. For location on i-th place will be dict like
         # {'direction1': location_in_direction1_behind_wall, ...}.
         # So all list looks like {loc1: {'dir1': loc_in_dir1, ...}, ...}.
-        # loc and loc_in_dir are integers (not location or ID) too.
 
     def break_wall(self):
         for loc in self.behind_the_wall:
