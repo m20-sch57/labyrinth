54a7b3d939c2bf9405473551d990819f611ac75b
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2019-05-09 17:12:10 +0300

    fixed disconnect handler error

--- app/room_namespace.py
+++ app/room_namespace.py
@@ -9,12 +9,14 @@ def init_room_namespaces():
 class RoomNamespace(Namespace):
     def on_connect(self):
         room_id = self.namespace[1:]
-        db.rooms.add_user(room_id)
-        emit('update', {'event': 'player_enter_or_leave', 'players': ','.join(db.rooms.get(room_id).users)},
-              broadcast = True, namespace = self.namespace)
+        if db.rooms.get(room_id):
+            db.rooms.add_user(room_id)
+            emit('update', {'event': 'player_enter_or_leave', 'players': ','.join(db.rooms.get(room_id).users)},
+                  broadcast = True, namespace = self.namespace)
 
     def on_disconnect(self):
         room_id = self.namespace[1:]
-        db.rooms.remove_user(room_id)
-        emit('update', {'event': 'player_enter_or_leave', 'players': ','.join(db.rooms.get(room_id).users)},
-              broadcast = True, namespace = self.namespace)
+        if db.rooms.get(room_id):
+            db.rooms.remove_user(room_id)
+            emit('update', {'event': 'player_enter_or_leave', 'players': ','.join(db.rooms.get(room_id).users)},
+                  broadcast = True, namespace = self.namespace)
