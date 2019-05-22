ac53759d6fbca81717fc99408039346a856a5e2a
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2019-05-22 17:55:53 +0300

    fixed placeholder

--- app/templates/js/contenteditable.js
+++ app/templates/js/contenteditable.js
@@ -1,12 +1,20 @@
 <script type='text/javascript'>
-    $('[contenteditable="plaintext-only"]').attr('contenteditable', 'true').addClass('contenteditable-plaintext-only')
+    $('[contenteditable="plaintext-only"]').attr('contenteditable', 'true').addClass('contenteditable-plaintext-only');
     $('.contenteditable-plaintext-only').on('paste', function(e) {
-         e.preventDefault();
-         
-         if( e.originalEvent.clipboardData ) {
-              var content = e.originalEvent.clipboardData.getData('text/plain').replace(/\r\n|\r|\n/g, '');
-              console.log('pasted content only text', content);
-              document.execCommand('insertText', false, content);
+        console.log('paste');
+        e.preventDefault();
+
+        if( e.originalEvent.clipboardData ) {
+            var content = $.trim(e.originalEvent.clipboardData.getData('text/plain'))
+            document.execCommand('insertText', false, content);
+        }
+    });
+    $(document).ready(function() {$('.contenteditable-plaintext-only').addClass('chat-input-show-placeholder');});
+    $('.contenteditable-plaintext-only').on('change paste keyup load', function() {
+        if ($(this).text()) {
+            $(this).removeClass('chat-input-show-placeholder');
+        } else {
+            $(this).addClass('chat-input-show-placeholder');
         }
     });
 </script>
